d484654ca13f8c6998ca48e374a025eae0c2655e
Roland <matleroland@gmail.com>
2024-06-04 18:35:20 +0200

    innerHTML megkezdése

--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,6 @@
 let form = document.querySelector(".js-form");
 let nameField = document.querySelector("[name=word]");
+let resultSection = document.querySelector(".js-result-container");
 
 function getApiUrl(word) {
     return `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
@@ -7,14 +8,24 @@ function getApiUrl(word) {
 
 function renderResponse(wordDefinitions) {
     console.log(wordDefinitions);
+
+    resultSection.innerHTML= `
+        <pre>
+            <code>
+                ${JSON.stringify(wordDefinitions)}
+            </code>
+        </Spre>
+    `;
 }
 
 function eseményKezelő(eseményObjektum) {
     eseményObjektum.preventDefault();
     let word = nameField.value.trim();
+
     console.log("Beírt érték: ", word);
 
     nameField.value = "";
+
     fetch(getApiUrl(word))
         .then(response => response.json())
         .then(renderResponse);
