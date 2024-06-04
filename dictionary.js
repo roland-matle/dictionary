215d47d3cb82cc7efb30f10e55373988d7293f5f
Roland <matleroland@gmail.com>
2024-06-04 19:52:14 +0200

    szó kiírás, fonetikusan is és definició

--- dictionary.js
+++ dictionary.js
@@ -6,16 +6,49 @@ function getApiUrl(word) {
     return `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 }
 
-function renderResponse(wordDefinitions) {
-    console.log(wordDefinitions);
+function renderDefinitions (definitions) {
+    let html = "";
+    for (let def of definitions){
+        html += `
+            <li>
+                ${def.definition} (e.g. ${def.example})
+            </li>
+        `;
+    }
 
-    resultSection.innerHTML= `
-        <pre>
-            <code>
-                ${JSON.stringify(wordDefinitions)}
-            </code>
-        </Spre>
+    return html;
+}
+
+function renderMeaning (meaning) {
+    let html = `
+        <p>${meaning.partOfSpeech}</p>
+        <ol>${renderDefinitions(meaning.definitions)}</ol>
     `;
+
+    return html;
+}
+
+function renderWord(word) {
+    let html = `<h2>${word.word} ${word.phonetic}</h2>`;
+    for (let meaning of word.meanings) {
+        html += renderMeaning(meaning);
+    }
+
+    return html;
+}
+
+function renderResponse(wordDefinitions) {
+    let html = "";
+
+    if (Array.isArray(wordDefinitions) && wordDefinitions.length > 0){
+        let definitions = wordDefinitions[0];
+        html = renderWord(definitions);
+    } else {
+        html = `<div class="error">The requested word does not exist.</div>`;
+    }
+
+    resultSection.innerHTML= html;
+
 }
 
 function eseményKezelő(eseményObjektum) {
